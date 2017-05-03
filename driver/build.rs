efb3902ac236713177e3e6222ad58d6e8f541a03
Paul Collier <paul@paulcollier.ca>
2017-05-02 22:05:07 -0400

    PWD-independent build scripts

--- driver/build.rs
+++ driver/build.rs
@@ -1,8 +1,14 @@
 use std::env;
+use std::path::PathBuf;
 
 fn main() {
     let profile = env::var("PROFILE").unwrap();
+    let mut deps = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap());
+    deps.pop();
+    deps.push("target");
+    deps.push(profile);
+    deps.push("deps");
 
     println!("cargo:rustc-link-lib=g");
-    println!("cargo:rustc-link-search=../target/{}/deps", profile);
+    println!("cargo:rustc-link-search={}", deps.display());
 }
