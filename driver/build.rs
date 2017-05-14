bca0f5bc387346d28fba773b8cf2295aebaa5594
Paul Collier <paul@paulcollier.ca>
2017-05-14 19:38:22 -0400

    Omit g link flags in oneshot builds

--- driver/build.rs
+++ driver/build.rs
@@ -2,16 +2,21 @@ use std::env;
 use std::path::PathBuf;
 
 fn main() {
-    let profile = env::var("PROFILE").unwrap();
-    let mut deps = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap());
-    deps.pop();
-    deps.push("g");
-    deps.push("target");
-    deps.push(profile);
-    deps.push("deps");
+    let is_static = env::var_os("CARGO_FEATURE_STATIC_GL").is_some() ||
+                    env::var_os("CARGO_FEATURE_STATIC_METAL").is_some();
 
-    println!("cargo:rustc-link-lib=g");
-    println!("cargo:rustc-link-search={}", deps.display());
+    if !is_static {
+        let profile = env::var("PROFILE").unwrap();
+        let mut deps = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap());
+        deps.pop();
+        deps.push("g");
+        deps.push("target");
+        deps.push(profile);
+        deps.push("deps");
+
+        println!("cargo:rustc-link-lib=g");
+        println!("cargo:rustc-link-search={}", deps.display());
+    }
 
     println!("cargo:rerun-if-changed=build.rs");
 }
